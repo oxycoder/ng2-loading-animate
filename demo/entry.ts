465e531203cb4907930e876a0023238006fcf3a2
oxycoder <oxycoder@gmail.com>
2016-09-17 11:20:59 +0700

    test(etc): fix test on 2.0.0 release

--- demo/entry.ts
+++ demo/entry.ts
@@ -1,4 +1,4 @@
-import 'reflect-metadata';
+import 'core-js';
 import 'zone.js/dist/zone';
 import {enableProdMode} from '@angular/core';
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
